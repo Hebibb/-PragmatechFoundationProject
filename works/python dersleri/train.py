20f34b4d9f12b2952d54107dce7cb24a5e0d577f
Hebibb <bhabibb1995@gmail.com>
2022-01-18 00:54:55 +0400

    initial python

---
+++ works/python dersleri/train.py
@@ -0,0 +1,21 @@
+a=11#variable
+print(a)#runs it on screen
+b=2.5
+c=type(11)#type int
+d=type(2.5)#float
+print(d)#shows int
+print(c)#shows float
+e=type('1')#
+print(type('1'))#str string
+print('space','between')#common between two str make space for them in execution
+a,b,c='true',True,12
+print(a,b,c)#you can define different variables at once
+
+#python list
+mix=[2,75,'sufle','strange things',5>75] #list in python can have any data type
+print(mix)
+#we can change it 
+mix[3]='qonaqlar'
+print(mix)
+#can slice it
+print(mix[0:2])
