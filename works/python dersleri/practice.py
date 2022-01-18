27207c96c5ab79e6fa29c34c1264922f11c27897
Hebibb <bhabibb1995@gmail.com>
2022-01-19 03:42:17 +0400

    ...

--- works/python dersleri/practice.py
+++ works/python dersleri/practice.py
@@ -1,14 +1,24 @@
 #FIRSTDAYOFPYTHON HOMEWORK
 # x=eval(input('input first operand:'))#evaluating default data type of builtin functions
 
-y=eval(input('second operand:')) 
-print(x+y)
-reqem=eval(input('give number'))
-print(type(reqem)==type(5))
-adimiz=input('adinizi daxil edin:')
-tersi=adimiz[::-1]
-print('adinizin tersi'+tersi)
+# y=eval(input('second operand:')) 
+# print(x+y)
+# reqem=eval(input('give number'))
+# print(type(reqem)==type(5))
+# adimiz=input('adinizi daxil edin:')
+# tersi=adimiz[::-1]
+# print('adinizin tersi'+tersi)
 boyukdur='TamAmDir'
-list(boyukdur)
+boyukler=list(boyukdur)
 
+
+
+for x in range(8):
+    boyuyun=boyukler[x].isupper()
+    c=0
+    print(c+boyuyun)
+    tekrarlar=c+boyuyun
+    print(type(boyuyun))
     
+    
+   
